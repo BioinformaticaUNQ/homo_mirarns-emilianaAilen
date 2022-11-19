8e883ec8f5989344b54c31e95d4a4f16f7ddcce1
Matias Fuentes <matiasnfuentes@gmail.com>
2022-11-19 20:00:30 -0300

    get alignments

--- homo_mirarns-base/homo_mirarns/Library.py
+++ homo_mirarns-base/homo_mirarns/Library.py
@@ -1,18 +1,35 @@
-from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIWWW, NCBIXML
 
 
-def hello_world():
-    print('hello world')
+def lookup_miRNAs(sequence, sequence_type, target_specie):
+    if sequence_type == "FATSA":
+        get_counterparts(sequence)
+    elif sequence_type == "MIRNA_FATSA":
+        get_counterparts(sequence)
+    else: 
+        "You're too young to party"
+
+def get_counterparts(sequence, E_VALUE_THRESH = 0.04):
+    result_handle  = NCBIWWW.qblast("blastn", "nt", sequence)
+    blast_record = NCBIXML.read(result_handle)
+    for alignment in blast_record.alignments:
+        for hsp in alignment.hsps:
+            if hsp.expect < E_VALUE_THRESH:
+                print("****Alignment****")
+                print("sequence:", alignment.title)
+                print("length:", alignment.length)
+                print("e value:", hsp.expect)
+                print(hsp.query[0:75] + "...")
+                print(hsp.match[0:75] + "...")
+                print(hsp.sbjct[0:75] + "...")   
+    return blast_record
+
+sequence_data = ">HO190899.1 Dn0d_1_1654 Dendrobium nobile un-vernalized cDNA library Dendrobium nobile cDNA 5', mRNA sequence  \n GGGGAGAGTAAAAGAATTGAGATTGAGACAAACAGCAGCAGTGGAAGGGGCATGCAGAGGAGCTCTATCAATTGGCAAGTGTGACAACGTCAAAGCTCCTATGTATGCCTCCTCCACTTCTGCTGCTAGCCTCCTATGCTACTGTTTGTTTTGATATTGTCAAGCGGCAACATCAAAGCTCTTAGTTTCTCTCGTTTATCTCTACTGTTAAGCATCTTAATCAGTGTTGGGTATGACTCTCGCTCCTATTTGATTAGATTTGAGAAACTTTTATATCGTTATACATATTATAGAAGTTTTATTTTAAATTTTTTTATAATCATGCGCGAATATAATAAAAGTTTTACCTACTTTTAATTTGTGATAATTTTAATTATCTTCTCTTGCTAGAAACTCTAATTTCATCACTGATCTTGACCTTTTGCTCATAAGCTCTCTGACTCGCTTTAAGGTTTTGTTTGAGACGGTTTTCTTATGCTTCTTAAAGCAGTTTTTTAGATTGAGAAGTTGATTTAAGAAACAGTAAGAATGACATCCCAAAGCCTAAGATTTCGTTTGGGATGGTTTTCTCACTGCTTTCTAGTAAAAAAATTCTGATTCAAGAAATTTTTATGCTGAAAAGCCGTTTTGTAAAGCAATAAGAAAGTTGTCTCAA"
+
+lookup_miRNAs(sequence_data, "MIRNA_FATSA", "asd")
 
 
-def lookup_miRNAs(code):
-    if code.startswith('>'):
-        return 'es una secuencia FASTA'
-    else:
-        return 'es un gene id'
-
-
-""" sequence_data = "›SeqABCD [organism=Mus musculus] [strain=C57BL/6]"
+""" 
 
 result_handle = NCBIWWW.qblast("blastn", "nt", sequence_data)
 print('termino')
@@ -21,6 +38,11 @@ print(result_handle)
 print(sequence_data)
 
 lookup_miRNAs('>asdasd')
+
+    if code.startswith('>'):
+        return 'es una secuencia FASTA'
+    else:
+        return 'es un gene id'
  """
 
  
